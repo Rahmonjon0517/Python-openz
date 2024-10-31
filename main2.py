f0b23bee45f343b684ab35fbf16885663b985d25
Rahmonjon <nematillayevrahmonjon@gmail.com>
2024-10-31 18:48:50 +0500

    PAndas
    PAndas

---
+++ main2.py
@@ -0,0 +1,13 @@
+import pandas as pd 
+Amaliyotustozlari= pd.DataFrame({ 'Ism' : [ 'Zulayho','Alisher','Mohigul','Gulnoza','Doniyor'], 
+                      'Familiya' : [ 'Qadamova ','Arabboyev','Abdullayeva', "Jo'rayeva" ,  "Mirkomilov"] , 
+                      'Yashash manzili' : [ "Farg'ona shaxar " , "Farg'ona shaxar " , "Farg'ona shaxar " , "Farg'ona shaxar " , "Farg'ona shaxar " ], 
+                       'Fan' : [ " Sun'iy intellekt","Kiberxavfsizlik","Ma'lumotlar tuzilomasi va algoritmlar", "Elektronika va sxemalar","Kompyuterni tashkil qilish"], 
+                       'Tel raqam' : [ '+998993022212', '+998993012212',  '+998993002212', '+998993002112', '+998993002312'], 
+ 
+ 
+ 
+ 
+ 
+}) 
+print (Amaliyotustozlari)
